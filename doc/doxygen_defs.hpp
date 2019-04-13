ed3f8dbe26fb931a63e77f1ebfc4e9ab0db2bb5d
cynodelic <alvaro-ceballos@hotmail.es>
2019-04-13 19:54:37 -0300

    Added proper documentation stuff

---
+++ doc/doxygen_defs.hpp
@@ -0,0 +1,64 @@
+// Copyright (c) 2019 Álvaro Ceballos
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt
+
+/**
+ * @defgroup types Types
+ */
+
+/**
+ * @defgroup pixeltypes Pixel types
+ * @ingroup types
+ */
+
+/**
+ * @defgroup exceptions Exceptions
+ * @ingroup types
+ */
+
+/**
+ * @defgroup typetraits Type traits
+ */
+
+/**
+ * @defgroup typeutils Type utilities
+ */
+
+/**
+ * @defgroup typecasting Type casting
+ * @ingroup typeutils
+ */
+
+/**
+ * @defgroup literals Literals
+ * @ingroup typeutils
+ */
+
+/**
+ * @defgroup imageio Image input/output
+ */
+
+/**
+ * @defgroup utilities Utilities
+ */
+
+/**
+ * @defgroup convert Image conversions
+ * @ingroup utilities
+ */
+
+/**
+ * @defgroup filters Image filters and effects
+ * @ingroup utilities
+ */
+
+/**
+ * @defgroup transformations Image transformations
+ * @ingroup utilities
+ */
+
+/**
+ * @defgroup imgscalingtags Tags for image scaling
+ * @ingroup transformations
+ */
+
